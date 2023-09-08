9c485c61430494d72d003884997a6f7b0cde817d
Victor <95230893+EdgnOfc@users.noreply.github.com>
2023-09-08 22:24:27 +0200

    Works

--- src/main/java/net/edgn/commands/ShowRaidCompletions.java
+++ src/main/java/net/edgn/commands/ShowRaidCompletions.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.command.CommandSource;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.NotNull;
@@ -18,7 +19,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class ShowRaidCompletions {
     private final static String TAG = Formatting.YELLOW + "[" + Formatting.GOLD + "ZAMN" + Formatting.YELLOW + "] ";
@@ -27,16 +27,22 @@ public class ShowRaidCompletions {
         LiteralArgumentBuilder<FabricClientCommandSource> edrCommand = LiteralArgumentBuilder.<FabricClientCommandSource>literal("edr")
                 .then(RequiredArgumentBuilder.<FabricClientCommandSource, String>argument("player", StringArgumentType.word())
                         .suggests((context, builder) -> {
-                            MinecraftServer server = context.getSource().getClient().getServer();
-                            assert server != null;
-                            PlayerManager playerManager = server.getPlayerManager();
-                            List<String> onlinePlayerNames = playerManager.getPlayerList().stream()
-                                    .map(player -> player.getName().getString())
-                                    .collect(Collectors.toList());
 
+                            List<String> onlinePlayerNames = new ArrayList<>();
+
+                            MinecraftServer server = context.getSource().getPlayer().getServer();
+
+                            assert(server != null);
+
+                            //PlayerManager playerManager = server.getPlayerManager();
                             onlinePlayerNames.add(context.getSource().getPlayer().getEntityName());
+                            /*for( ServerPlayerEntity spe : playerManager.getPlayerList()) {
+                                if (spe.getName().toString().equals(context.getSource().getPlayer().getEntityName())) continue;
+                                onlinePlayerNames.add(spe.getName().toString());
+                            }*/
 
                             return CommandSource.suggestMatching(onlinePlayerNames, builder);
+
                         })
                         .executes(context -> execute(context.getSource(), StringArgumentType.getString(context, "player"))));
 
@@ -44,48 +50,51 @@ public class ShowRaidCompletions {
     }
 
     private static int execute(FabricClientCommandSource source, String playerName) {
-        List<String> raidNames = Arrays.asList("The Nameless Anomaly", "The Canyon Colossus",
-                "Orphion's Nexus of Light", "Nest of the Grootslangs");
-        List<String> raidLabels = Arrays.asList("TNA", "TCC", "NOL", "NOTG");
-        List<Integer> finalRaidCompletions = new ArrayList<>();
+        final Thread myThread = new Thread(() -> {
+            List<String> raidNames = Arrays.asList("The Nameless Anomaly", "The Canyon Colossus",
+                    "Orphion's Nexus of Light", "Nest of the Grootslangs");
+            List<String> raidLabels = Arrays.asList("TNA", "TCC", "NOL", "NOTG");
+            List<Integer> finalRaidCompletions = new ArrayList<>();
 
-        source.sendFeedback(Text.literal(TAG)
-                .append(Text.translatable("commands.edgnmod.show_raid_completions.header")
-                        .styled(style -> style.withColor(Formatting.GOLD))
-                        .append(Text.literal(playerName)
-                                .styled(style -> style.withBold(true).withColor(Formatting.RED)))));
+            source.sendFeedback(Text.literal(TAG)
+                    .append(Text.translatable("commands.edgnmod.show_raid_completions.header")
+                            .styled(style -> style.withColor(Formatting.GOLD))
+                            .append(Text.literal(playerName)
+                                    .styled(style -> style.withBold(true).withColor(Formatting.RED)))));
 
-        String result = WynnApiUtils.getStringFromURL("https://api.wynncraft.com/v2/player/" + playerName + "/stats");
+            String result = WynnApiUtils.getStringFromURL("https://api.wynncraft.com/v2/player/" + playerName + "/stats");
 
-        for (int i = 0; i < raidNames.size(); i++) {
-            int completion = 0;
+            for (int i = 0; i < raidNames.size(); i++) {
+                int completion = 0;
 
-            String regex = raidNames.get(i) + "\",\"completed\":([0-9]*)";
-            Matcher matcher = Pattern.compile(regex).matcher(result);
-            while (matcher.find()) {
-                completion += Integer.parseInt(matcher.group(1));
+                String regex = raidNames.get(i) + "\",\"completed\":([0-9]*)";
+                Matcher matcher = Pattern.compile(regex).matcher(result);
+                while (matcher.find()) {
+                    completion += Integer.parseInt(matcher.group(1));
+                }
+
+                finalRaidCompletions.add(completion);
+
+                source.sendFeedback(Text.literal(Formatting.GOLD + "- ")
+                        .append(Text.translatable("commands.edgnmod.show_raid_completions.entry",
+                                Text.literal(raidLabels.get(i)).styled(style -> style.withColor(Formatting.AQUA)),
+                                Text.literal(": ").styled(style -> style.withColor(Formatting.AQUA)),
+                                Text.literal(String.valueOf(finalRaidCompletions.get(i)))
+                                        .styled(style -> style.withColor(Formatting.GOLD)),
+                                Text.translatable("commands.edgnmod.show_raid_completions.times")
+                                        .styled(style -> style.withColor(Formatting.YELLOW)))));
             }
 
-            finalRaidCompletions.add(completion);
-
+            int sum = finalRaidCompletions.stream().mapToInt(Integer::intValue).sum();
             source.sendFeedback(Text.literal(Formatting.GOLD + "- ")
-                    .append(Text.translatable("commands.edgnmod.show_raid_completions.entry",
-                            Text.literal(raidLabels.get(i)).styled(style -> style.withColor(Formatting.AQUA)),
-                            Text.literal(": ").styled(style -> style.withColor(Formatting.AQUA)),
-                            Text.literal(String.valueOf(finalRaidCompletions.get(i)))
-                                    .styled(style -> style.withColor(Formatting.GOLD)),
-                            Text.translatable("commands.edgnmod.show_raid_completions.times")
-                                    .styled(style -> style.withColor(Formatting.YELLOW)))));
-        }
-
-        int sum = finalRaidCompletions.stream().mapToInt(Integer::intValue).sum();
-        source.sendFeedback(Text.literal(Formatting.GOLD + "- ")
-                .append(Text.translatable("commands.edgnmod.show_raid_completions.total")
-                        .styled(style -> style.withColor(Formatting.AQUA))
-                        .append(Formatting.AQUA + ": ")
-                        .append(Text.literal(Formatting.GOLD + String.valueOf(sum))
-                                        .styled(style -> style.withColor(Formatting.GOLD))
-                        )));
+                    .append(Text.translatable("commands.edgnmod.show_raid_completions.total")
+                            .styled(style -> style.withColor(Formatting.AQUA))
+                            .append(Formatting.AQUA + ": ")
+                            .append(Text.literal(Formatting.GOLD + String.valueOf(sum))
+                                    .styled(style -> style.withColor(Formatting.GOLD))
+                            )));
+        });
+        myThread.start();
 
         return 1;
     }
